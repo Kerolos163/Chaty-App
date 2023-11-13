e8a1f2fc1c1b3eb6fcacd8e915ff85f04c8185f7
Kerolos163 <kokoessa951@gmail.com>
2023-11-13 19:16:55 +0200

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'Core/utils/color_manager.dart';
 import 'Features/Splash/presentation/view/splash_view.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -25,4 +27,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
